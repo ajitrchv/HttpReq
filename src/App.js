02b13afac1ed8cf7a87f97111f3c19545c713d15
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-03-07 16:13:56 +0530

    added automated fetching

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import React, {useState} from "react";
-
+import React, {useState, useEffect, useCallback} from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading,setIsLoading] = useState(false);
+  const [error,setError] = useState(null);
 
-   async function fetchMoviesHandler(){ 
+
+   const fetchMoviesHandler = useCallback(async () => { 
 
     setIsLoading(true);
+    setError(null);
+    try{
 
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+      const response = await fetch("https://swapi.dev/api/films");
 
-    const transformedMovies =data.results.map(
-          movieData =>{
-            return {
-              id: movieData.episode_id,
-              title: movieData.title,
-              openingText: movieData.opening_crawl,
-              releaseDate: movieData.release_date
-            };
-          }
-        );
-        setMovies(transformedMovies);
+      if(!response.ok)
+      {
+        throw new Error('Something went Wrong!')
+      }
 
-        setIsLoading(false);
-    };
+      const data = await response.json();
+
+  
+      const transformedMovies =data.results.map(
+            movieData =>{
+              return {
+                id: movieData.episode_id,
+                title: movieData.title,
+                openingText: movieData.opening_crawl,
+                releaseDate: movieData.release_date
+              };
+            }
+          );
+          setMovies(transformedMovies);
+  
+      }catch(error)
+      {
+          setError(error.message);
+      }
+      setIsLoading(false);
+
+    },[]);
+
+
+    useEffect(() => {
+      fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
@@ -36,8 +56,9 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found No Movies</p>}
+        {!isLoading && movies.length === 0 && !error && <p>Found No Movies</p>}
         {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
